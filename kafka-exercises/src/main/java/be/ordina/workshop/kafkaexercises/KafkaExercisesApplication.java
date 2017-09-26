62dfd2fd6893fbe1b0a8112c65605a5ccdcbdcf7
Tom Van den Bulck <tom.vandenbulck@ordina.be>
2017-09-26 22:40:21 +0200

    some cleanup of the copied join content

---
+++ kafka-exercises/src/main/java/be/ordina/workshop/kafkaexercises/KafkaExercisesApplication.java
@@ -0,0 +1,12 @@
+package be.ordina.workshop.kafkaexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExercisesApplication.class, args);
+	}
+}
